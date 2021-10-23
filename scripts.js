6631a62c726dfbabc19d5a71b7dc14ec1f2cc769
Courtney Brown <courtnhey.brown@gmail.com>
2021-10-22 21:17:38 -0500

    Some code added to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
-/* Add your JavaScript to this file */
+window.onload = function(){
+    subscription();
+}
+
+function subscription(){
+    
+    let button = document.querySelector('form');
+    let btn = button.getElementsByTagName('button');
+    let msg = document.getElementsByClassName('message');
+    let emailAddress = document.getElementsByTagName('input').value
+
+
+    btn.onclick =  function() {subsribe()};
+
+    function subsribe(){
+        alert('is this thing on??')
+            function validateEmail(email){
+                let regex = /\S+@\S+\.\S+/;
+                return regex.test(email);
+                    }
+    
+    if (validateEmail(emailAddress== true)) {
+            msg.innerText = 'Thank you! Your email address' + emailAddress + 'has been added to our mailing list' ;
+        
+    }
+
+    else{
+
+        msg.innerText = 'Please enter a valid email address'
+    }
+
+    }
+}
